72949436275c4836b366c2289ec1a7308eff4b05
NickMcLarnan1104 <101211136+NickMcLarnan1104@users.noreply.github.com>
2022-09-22 19:26:26 -0400

    Add files via upload

---
+++ PersonClass/mclarnan_nick_exam2/Person.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+    Nicholas McLarnan
+    Exam 2
+    Create a program using properties, instance variables, methods that will 
+                tell user how many years until they retire
+    07/20/2022
+*/
+
+// create Person class
+class Person
+{
+    // declare empty name variable
+    string name = "";
+    // declare empty age variable
+    int age;
+
+
+    // Name property
+    public string Name
+    {
+        get { return name; } // get name
+
+        set { name = value; } // set name
+    }
+
+
+
+    // Age property
+    public int Age
+    {
+        get { return age; } // get age
+
+        set { age = value; } // set age
+
+    }
+
+    // declare empty yearsToWork variable
+    int yearsToWork;
+
+    // YearsToWork property
+    public int YearsToWork
+    {
+        get { return yearsToWork; } // get yearsToWork
+
+        set { yearsToWork = value; } // set yearsToWork
+    }
+
+    // YTK method
+    public int YTK(int Age)
+    {
+        // return years until you can retire
+        return YearsToWork = 65 - Age;
+    } // end YTK method
+
+
+} // end Person class
+
+// END PROGRAM
---
+++ PersonClass/mclarnan_nick_exam2/PersonRun.cs
@@ -0,0 +1,39 @@
+﻿/*
+
+    Nicholas McLarnan
+    Exam 2
+    Create a program using properties, instance variables, methods that will 
+                tell user how many years until they retire
+    07/20/2022
+*/
+
+// create PersonRun class
+class PersonRun
+{
+    // create Main method
+    static void Main()
+    {
+        // create a new instance variable
+        Person person = new Person();
+
+        // input user's name
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        // input user's age
+        Console.Write("Pleaee enter your age: ");
+        int age = Convert.ToInt32( Console.ReadLine() );
+
+        // assign yearsToWork to person.YTK() with age argument
+        int yearsToWork = person.YTK(age);
+
+        // display results
+        Console.WriteLine();
+        Console.WriteLine("{0}, you  will work: {1} years before you retire.", name, yearsToWork);
+
+
+    } // end Main method
+    
+} // end PersonRun class
+
+// END PROGRAM
